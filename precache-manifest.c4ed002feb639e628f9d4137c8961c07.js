ab4f54fdfb953a2c8e7bb7de614ca3ef799ef056
qoobot <g@qoobot.ru>
2019-12-09 19:17:24 +0700

    Updates

---
+++ precache-manifest.c4ed002feb639e628f9d4137c8961c07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fe3457a8db1df3f68537ab393ca029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "619d5befaf91bcf806ae",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "64eb18ba971309bd37d3",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "619d5befaf91bcf806ae",
+    "url": "/static/js/2.0c780e4e.chunk.js"
+  },
+  {
+    "revision": "64eb18ba971309bd37d3",
+    "url": "/static/js/main.1fa08a8b.chunk.js"
+  },
+  {
+    "revision": "092ad0b94253b6b77918",
+    "url": "/static/js/runtime-main.1ab7242a.js"
+  },
+  {
+    "revision": "ad9e902128bf287437eaf2b515c1bbf4",
+    "url": "/static/media/persik.ad9e9021.png"
+  }
+]);
